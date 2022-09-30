c5079cfc2739e60ab77231ff12ad72312dbb7a02
SukkaW <isukkaw@gmail.com>
2022-09-30 21:37:41 +0800

    [server] add build step

---
+++ packages/server/rollup.config.ts
@@ -0,0 +1,32 @@
+import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+import { defineConfig } from 'rollup';
+
+import * as pkgJson from './package.json';
+
+const option = defineConfig({
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  external: Object.keys(pkgJson.dependencies),
+  plugins: [
+    commonjs({
+      esmExternals: true
+    }),
+    nodeResolve({
+      exportConditions: ['import', 'require', 'default']
+    }),
+    swc(defineRollupSwcOption({
+      jsc: {
+        externalHelpers: false,
+        target: 'es2018'
+      }
+    }))
+  ]
+});
+
+export default option;
