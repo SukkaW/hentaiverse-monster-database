16aa3443b3a7de70cf19017c54dd60e94ea355e4
SukkaW <isukkaw@gmail.com>
2024-02-01 13:13:06 +0800

    chore: housekeeping

--- packages/api-server-on-vercel/api/monsterdata.ts
+++ packages/api-server-on-vercel/api/monsterdata.ts
@@ -10,7 +10,6 @@ if (typeof process.env.DETA_PROJECT_KEY !== 'string') {
 const fastify = Fastify({});
 fastify.register(app);
 
-export default async (request: VercelRequest, response: VercelResponse) => {
-  await fastify.ready();
-  fastify.server.emit('request', request, response);
+export default (request: VercelRequest, response: VercelResponse) => {
+  fastify.ready().then(() => fastify.server.emit('request', request, response));
 };
