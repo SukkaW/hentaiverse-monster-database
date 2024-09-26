575822ff97bcfa541722ab7737e40f2a1fbc372d
SukkaW <isukkaw@gmail.com>
2024-09-26 21:29:19 +0800

    feat(data): implement pull from supabase

--- packages/data/tools/import.ts
+++ packages/data/tools/import.ts
@@ -9,16 +9,16 @@ if (typeof process.env.SUPABASE_PROJECT_URL !== 'string') {
   dotenv.config({ path: findUp.sync('.env') });
 }
 
-if (
-  typeof process.env.SUPABASE_PROJECT_URL !== 'string'
-  || typeof process.env.SUPABASE_SERVICE_KEY !== 'string'
-) {
-  throw new TypeError('DETA_PROJECT_KEY is not defined!');
-}
-
 (async () => {
+  if (
+    typeof process.env.SUPABASE_PROJECT_URL !== 'string'
+    || typeof process.env.SUPABASE_SERVICE_KEY !== 'string'
+  ) {
+    throw new TypeError('SUPABASE_PROJECT_URL or SUPABASE_SERVICE_KEY is not defined!');
+  }
+
   // Create a single supabase client for interacting with your database
-  const supabase = createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SUPABASE_SERVICE_KEY!);
+  const supabase = createClient(process.env.SUPABASE_PROJECT_URL, process.env.SUPABASE_SERVICE_KEY);
   const [persistentData, isekaiData] = await Promise.all([
     fsp.readFile(path.join(__dirname, 'persistent.json'), 'utf8').then(JSON.parse),
     fsp.readFile(path.join(__dirname, 'isekai.json'), 'utf8').then(JSON.parse)
